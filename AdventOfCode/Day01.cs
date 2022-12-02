5a30710b7ca52a044a4719c0fdef11cbbf9e8a59
Sunny Singh <sunnysingh@posteo.de>
2022-12-02 12:52:10 +0100

    Initial commit

---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode;
+
+public static class Day01
+{
+    public static int CalculateTheTotalOfTopElve(string path)
+    {
+        int max = 0;
+        int calories = 0;
+        foreach (string line in File.ReadLines(path))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                max = max < calories ? calories : max;
+                calories = 0;
+                continue;
+            }
+
+            if (int.TryParse(line, out int value))
+            {
+                calories += value;
+            }
+        }
+
+        max = max < calories ? calories : max;
+
+        return max;
+    }
+
+    public static int CalculateTheTotalOfTopElves(string path, int take)
+    {
+        SortedSet<int> totals = new SortedSet<int>();
+        int calories = 0;
+        foreach (string line in File.ReadLines(path))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                totals.Add(calories);
+                calories = 0;
+                continue;
+            }
+
+            if (int.TryParse(line, out int value))
+            {
+                calories += value;
+            }
+        }
+
+        if(calories != 0)
+        {
+            totals.Add(calories);
+        }
+
+        return totals.TakeLast(take).Sum();
+    }
+}
