d7bd97490bcdfffa9bb50f0e678cca7cac7df84a
Sunny Singh <sunnysingh@posteo.de>
2022-12-15 12:13:44 +0100

    Solves day 05 part 2

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public static class Day05
 {
@@ -67,7 +65,63 @@ public static class Day05
 
     public static string Part02(string path)
     {
-        return "";
+        string result = "";
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+
+            line = reader.ReadLine() ?? "";
+            int numberOfStacks = (line.Length + 1) / 4;
+
+            List<char>[] stacks = new List<char>[numberOfStacks];
+
+            for (int i = 0; i < numberOfStacks; i++)
+            {
+                stacks[i] = new List<char>();
+            }
+
+            do
+            {
+                if (line.StartsWith(" 1")) break;
+
+                for (int i = 0; i < numberOfStacks; i++)
+                {
+                    char c = line[(i * 4) + 1];
+                    if (char.IsLetter(c))
+                    {
+                        stacks[i].Add(c);
+                    }
+                }
+            } while ((line = reader.ReadLine()) != null);
+
+            for (int i = 0; i < numberOfStacks; i++)
+            {
+                stacks[i].Reverse();
+            }
+
+            reader.ReadLine();// empty line
+
+            string[] splitted;
+            int move, from, to;
+            while ((line = reader.ReadLine()) != null)
+            {
+                splitted = line.Split(' ');
+                move = int.Parse(splitted[1]);
+                from = int.Parse(splitted[3]) - 1;
+                to = int.Parse(splitted[5]) - 1;
+
+                stacks[to].AddRange(stacks[from].TakeLast(move));
+                stacks[from].RemoveRange(stacks[from].Count - move, move);
+            }
+
+            for (int i = 0; i < numberOfStacks; i++)
+            {
+                result += stacks[i].Last();
+            }
+        }
+
+        return result;
     }
 
 }
--- Tests/TestDay05.cs
+++ Tests/TestDay05.cs
@@ -15,15 +15,15 @@ namespace Tests
             Assert.That(Day05.Part01(path), Is.EqualTo(result));
         }
 
-        //[Test]
-        //[TestCase(@"../../../Files/05ExampleInput.txt", "CMZ")]
-        //[TestCase(@"../../../Files/05Input.txt", "ABC")]
-        //public void Test_Day05_Part02(
-        //string path,
-        //string result
-        //)
-        //{
-        //    Assert.That(Day05.Part02(path), Is.EqualTo(result));
-        //}
+        [Test]
+        [TestCase(@"../../../Files/05ExampleInput.txt", "MCD")]
+        [TestCase(@"../../../Files/05Input.txt", "ABC")]
+        public void Test_Day05_Part02(
+        string path,
+        string result
+        )
+        {
+            Assert.That(Day05.Part02(path), Is.EqualTo(result));
+        }
     }
 }
